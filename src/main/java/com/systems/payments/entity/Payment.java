c46796119da6b21ff5a2993eb8b786e703e62229
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-03-16 20:59:23 -0500

    subo entity con payment

--- src/main/java/com/systems/payments/entity/Payment.java
+++ src/main/java/com/systems/payments/entity/Payment.java
@@ -1,5 +1,41 @@
 package com.systems.payments.entity;
 
+import java.time.LocalDate;
+
+import com.systems.payments.entity.enums.PaymentStatus;
+import com.systems.payments.entity.enums.TypePayment;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder // Su función principal es facilitar la creación de objetos complejos sin tener
+         // que lidiar con constructores gigantes o muchas líneas de código "setter".
+@NoArgsConstructor
+@AllArgsConstructor
 public class Payment {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private String id;
+
+        private LocalDate date;
+
+        private double amount;
+
+        private TypePayment typePayment;
+
+        private PaymentStatus paymentStatus;
+
+        private String file;
+
+        @ManyToOne
+        private Student student;
 }
